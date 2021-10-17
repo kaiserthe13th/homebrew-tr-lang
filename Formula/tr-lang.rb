b4ae27ee70a351cbe48bb5dc2ca36d9595f8f95c
Kerem Göksu <superkerem13@gmail.com>
2021-10-17 18:44:24 +0300

    fix syntax for 0.1.3

--- Formula/tr-lang.rb
+++ Formula/tr-lang.rb
@@ -6,7 +6,6 @@ class TrLang < Formula
   homepage "https://github.com/kaiserthe13th/tr-lang"
   url "https://github.com/kaiserthe13th/tr-lang/archive/refs/tags/tr-lang-0.1.3.tar.gz"
   sha256 "a5026340cadaba61e4f44cbddcdbe81b0de3c09f6be1f49396a3fe25a7686e2b"
-  licence "MIT"
   version "0.1.3"
   depends_on "rust" => :build
 
