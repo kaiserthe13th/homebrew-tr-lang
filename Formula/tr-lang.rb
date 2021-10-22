cb3b450f967605bf147d2aeacb2d6b5425a78d72
Kerem Göksu <superkerem13@gmail.com>
2021-10-22 22:45:13 +0300

    Version 0.2.2

--- Formula/tr-lang.rb
+++ Formula/tr-lang.rb
@@ -4,8 +4,8 @@
 class TrLang < Formula
   desc "A programming language that aims to bring syntax closer to Turkish" 
   homepage "https://github.com/kaiserthe13th/tr-lang"
-  url "https://github.com/kaiserthe13th/tr-lang/archive/refs/tags/tr-lang-0.2.0.tar.gz"
-  sha256 "1f7b515ad035109da36e0ce1fbf7303f81002b154cc303c80e37463852b05a7d"
+  url "https://github.com/kaiserthe13th/tr-lang/archive/refs/tags/tr-lang-0.2.2.tar.gz"
+  sha256 "364e868ed1fef4653b90b351dd07a38e835fa71e29ab489e64a5dc211f646973"
   version "0.2.0"
   license "MIT"
   depends_on "rust" => :build
